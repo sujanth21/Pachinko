90de67181df0172f5a5abc1b89ab6ad15abc78c6
Sujanth <sujanth1987@gmail.com>
2017-08-29 13:28:32 +1000

    background and objects added

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -12,10 +12,23 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            box.position = location
+            addChild(box)
+        }
     }
 }
